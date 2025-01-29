36f78cdbbcbf6ca6fdce67e1fcec6ddd64449e7b
Edt12 <104518243+Edt12@users.noreply.github.com>
2025-01-29 10:42:38 +0000

    Reworked Bishop.java checkmovement to work in same way as queens detects movement is calculate using traversal method from Main.java

--- src/Bishop.java
+++ src/Bishop.java
@@ -1,6 +1,8 @@
 import javax.swing.*;
 
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 import static java.lang.Math.sqrt;
 
@@ -19,165 +21,32 @@ public class Bishop extends Piece {
      * is a piece between the start and the end and if there is it returns false and stops movement past that point
      */
     public boolean checkMovement(int[] movementCoords,Piece[][] pieces,BlankSquare[][] blankSquares) {
-        System.out.println("Starting CHecks");
         int y1 =movementCoords[0];
         int x1 = movementCoords[1];
         int y2 = movementCoords[2];
         int x2 = movementCoords[3];
-        int opposingPieceCounter = 0;//Keeps track of if a piece of the opposing colour is in this pieces path
-        int originalX2 = x2;
-        int originalY2 = y2;
 
-        System.out.println(y1 + " y1 " + y2 + "y2");
+        ArrayList<Pair> diagonalMovesOne = Main.traversal(y1,x1,1,1,pieces);
+        ArrayList<Pair> diagonalMovesTwo = Main.traversal(y1,x1,1,-1,pieces);
+        ArrayList<Pair> diagonalMovesThree = Main.traversal(y1,x1,-1,1,pieces);
+        ArrayList<Pair> diagonalMovesFour = Main.traversal(y1,x1,-1,-1,pieces);
 
-        if ((y1 != y2 && x1 != x2)
-                && (((x1 - x2) == (y1 -y2))||
-                ((x1 - x2) ==(y1 - y2) * -1))||
-                (((x1 - x2) * -1) == (y1 - y2))){
+        ArrayList<Pair> validMoves = new ArrayList<>(diagonalMovesOne);
+        validMoves.addAll(diagonalMovesOne);
+        validMoves.addAll(diagonalMovesTwo);
+        validMoves.addAll(diagonalMovesThree);
+        validMoves.addAll(diagonalMovesFour);
 
-            
-            System.out.println(sqrt((y1 - y2) * (y1 -y2)) + " FOR LOOP BISHOP CHECK");
-            double positiveDifferenceY1AndY2 = sqrt((y1 - y2) * (y1 -y2));
-            for (int i = 0; i < positiveDifferenceY1AndY2;i++) {
+        ArrayList<Pair> validMovesSearch = validMoves.stream()
+                .filter(x -> x.equals(new Pair(y2,x2)))
+                .collect(Collectors.toCollection(ArrayList<Pair>::new));
+        System.out.println(validMovesSearch.size() + "LIST SIZE");
 
-                if (y1 - y2 < 0 && x1 - x2 < 0) {
-                    System.out.println("1");
-                    if (pieces[y1][x1].isOrange()) {
-                        if (y2 == originalY2 && x2 == originalX2 && pieces[y2][x2] != null && !pieces[y2][x2].isOrange()){
-                            return true;
-                        }
-
-                        if (pieces[y2][x2] != null && !pieces[y2][x2].isOrange()) {
-                            System.out.println("Piece found");
-                            return false;
-
-
-                        } else if (pieces[y2][x2] != null && pieces[y2][x2].isOrange()) {
-                            System.out.println("Piece Found");
-                            return false;
-                        }
-                    } else if (!pieces[y1][x1].isOrange()) {
-
-                        if (y2 == originalY2 && x2 == originalX2 && pieces[y2][x2] != null && pieces[y2][x2].isOrange()){
-                            return true;
-                        }
-                        if (pieces[y2][x2] != null && pieces[y2][x2].isOrange()) {
-                            System.out.println("Piece found");
-                            return false;
-
-                        } else if (pieces[y2][x2] != null && !pieces[y2][x2].isOrange()) {
-                            System.out.println("Piece found");
-                            return false;
-                        }
-                    }
-
-                    y2 = y2 - 1;
-                    x2 = x2 - 1;
-                } else if (y1 - y2 > 0 && x1 - x2 > 0) {
-                    System.out.println("2");
-
-                    if (pieces[y1][x1].isOrange()) {
-
-                        if (y2 == originalY2 && x2 == originalX2 && pieces[y2][x2] != null && !pieces[y2][x2].isOrange()){
-                            return true;
-                        }
-                        if (pieces[y2][x2] != null && !pieces[y2][x2].isOrange()) {
-                            System.out.println("Piece found");
-                            return false;
-                        } else if (pieces[y2][x2] != null && pieces[y2][x2].isOrange()) {
-                            System.out.println("Piece Found");
-                            return false;
-                        }
-                    } else if (!pieces[y1][x1].isOrange()) {
-
-                        if (y2 == originalY2 && x2 == originalX2 && pieces[y2][x2] != null && pieces[y2][x2].isOrange()){
-                            return true;
-                        }
-                        if (pieces[y2][x2] != null && pieces[y2][x2].isOrange()) {
-                            System.out.println("Piece found");
-                            return false;
-                        } else if (pieces[y2][x2] != null && !pieces[y2][x2].isOrange()) {
-                            System.out.println("Piece found");
-                            return false;
-                        }
-                    }
-                    y2 = y2 + 1;
-                    x2 = x2 + 1;
-
-                } else if (y1 - y2 < 0 && x1 - x2 > 0) {
-                    System.out.println("Y2 " + y2);
-                    System.out.println("X2 " + x2);
-                    System.out.println("3");
-
-                    if (pieces[y1][x1].isOrange()) {
-
-                        if (y2 == originalY2 && x2 == originalX2 && pieces[y2][x2] != null && !pieces[y2][x2].isOrange()){
-                            return true;
-                        }
-                        if (pieces[y2][x2] != null && !pieces[y2][x2].isOrange()) {
-                            System.out.println("Piece found");
-                            return false;
-                        } else if (pieces[y2][x2] != null && pieces[y2][x2].isOrange()) {
-                            System.out.println("Piece found");
-                            return false;
-
-                        }
-                    } else if (!pieces[y1][x1].isOrange()) {
-                        if (y2 == originalY2 && x2 == originalX2 && pieces[y2][x2] != null && pieces[y2][x2].isOrange()){
-                            return true;
-                        }
-
-                        if (pieces[y2][x2] != null && pieces[y2][x2].isOrange()) {
-                            System.out.println("Piece found");
-                            return false;
-                        } else if (pieces[y2][x2] != null && !pieces[y2][x2].isOrange()) {
-                            System.out.println("Piece found");
-                            return false;
-                        }
-                        y2 = y2 - 1;
-                        x2 = x2 + 1;
-                    } else if (y1 - y2 > 0 && x1 - x2 < 0) {
-                        System.out.println("4");
-                        if (pieces[y1][x1].isOrange()) {
-
-                            if (y2 == originalY2 && x2 == originalX2 && pieces[y2][x2] != null && !pieces[y2][x2].isOrange()){
-                                return true;
-                            }
-                            if (pieces[y2][x2] != null && !pieces[y2][x2].isOrange()) {
-                                System.out.println("Piece found");
-                                return false;
-
-                            } else if (pieces[y2][x2] != null && pieces[y2][x2].isOrange()) {
-                                System.out.println("Piece found");
-                                return false;
-                            }
-
-                        } else if (!pieces[y1][x1].isOrange()) {
-
-                            if (y2 == originalY2 && x2 == originalX2 && pieces[y2][x2] != null && pieces[y2][x2].isOrange()){
-                                return true;
-                            }
-                            if (pieces[y2][x2] != null && pieces[y2][x2].isOrange()) {
-                                System.out.println("Piece found");
-                                return false;
-
-                            } else if (pieces[y2][x2] != null && !pieces[y2][x2].isOrange()) {
-                                System.out.println("Piece found");
-                                return false;
-
-                            }
-                        }
-                        y2 = y2 + 1;
-                        x2 = x2 - 1;
-                    }
-                    System.out.println(opposingPieceCounter + " Opposing Piece Counter");
-                }
-            }
+        if (validMovesSearch.size() >= 1){
             return true;
         }else {
             return false;
         }
-
     }
 }
 
--- src/Queen.java
+++ src/Queen.java
@@ -18,23 +18,6 @@ public class Queen extends Piece {
         int x1 = movementCoords[1];
         int y2 = movementCoords[2];
         int x2 = movementCoords[3];
-        System.out.println(y1 + " Y QUEEN");
-        System.out.println(x1 + " X QUEEN");
-        //TODO REWORK COLLIDE SEE IPAD WITH COLLIDE BUT CHANGE TO RETURN LIST OF SQUARES INSTEAD OF BOOLEAN
-        /*
-
-
-        if (x1 - x2 == 0 || y1 - y2 == 0
-            || x1 - x2 == 1 && y1 - y2 == 1
-            || x1 - x2 == -1 && y1 - y2 == -1
-            || x1 - x2 == 1 && y1 - y2 == -1
-            || x1 - x2 == -1 && y1 - y2 == 1
-            || y1 != y2 && x1 != x2
-              && x1 - x2 == y1 -y2||
-              x1 - x2 ==y1 - y2 * -1||
-              (x1 - x2) * -1 == (y1 - y2)){
-            
-        */
 
         ArrayList<Pair> verticalMovesOne = Main.traversal(y1,x1,1,0,pieces);
         ArrayList<Pair> verticalMovesTwo = Main.traversal(y1,x1,-1,0,pieces);
@@ -58,10 +41,7 @@ public class Queen extends Piece {
                 .filter(x -> x.equals(new Pair(y2,x2)))
                 .collect(Collectors.toCollection(ArrayList<Pair>::new));
         System.out.println(validMovesSearch.size() + "LIST SIZE");
-        for(Pair elem : validMovesSearch) {
-            System.out.println(elem.getX() + " " + elem.getY());
-        }
-        System.out.println(y2 + " " + x2 + "Coordinates");
+
         if (validMovesSearch.size() >= 1){
             return true;
         }else {
