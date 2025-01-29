f843797e8a9b700787dbb1d06f48a89fa20833e5
Edt12 <104518243+Edt12@users.noreply.github.com>
2025-01-29 13:05:38 +0000

    Fixed Knight Movement

--- src/Knight.java
+++ src/Knight.java
@@ -9,21 +9,18 @@ public class Knight extends Piece{
     }
     @Override
     public boolean checkMovement(int[] movementCoords, Piece[][] pieces,BlankSquare[][] blankSquares){
-        int x1 =movementCoords[0];
-        int y1 = movementCoords[1];
-        int x2 = movementCoords[2];
-        int y2 = movementCoords[3];
-        if(x1 - x2 == 2 && y1 - y2 == 1||
-            x1 - x2 == 2 && y1 - y2 == -1||
-            x1 - x2 == -2 && x1 - y2 == -1||
-            x1 - x2 == -2 && y1 - y2 == 1){
-            return true;
-
-        }else {
-
-            return false;
+        int y1 =movementCoords[0];
+        int x1 = movementCoords[1];
+        int y2 = movementCoords[2];
+        int x2 = movementCoords[3];
+        Pair [] possibleMoves = {new Pair(2,1),new Pair(2,-1),new Pair(-2,1),new Pair(-2,-1),
+                new Pair(1,2),new Pair(1,-2),new Pair(-1,2),new Pair(-1,-2)};
+        for(int i = 0;i < possibleMoves.length;i++){
+            if (y2 == possibleMoves[i].getY() + y1 && x2 == possibleMoves[i].getX() + x1){
+                return true;
+            }
 
         }
-
+        return false;
     }
 }
